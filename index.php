fdad8a97e23a914945fd193c258790d26f830cca
PedroCororatte <pedroagt112@gmail.com>
2023-09-12 00:21:33 -0300

    update in main page

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php
-    echo "This is PhotoVault!";
-    echo "v0.1";
+$imageDirectory = "images/";
+$images = glob($imageDirectory . "*.{jpg,jpeg,png,gif}", GLOB_BRACE);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>PhotoVault</title>
+</head>
+
+<body>
+    <div class="gallery">
+        <?php
+            foreach($images as $image):?>
+        <a href="<?php echo htmlspecialchars($image); ?>" target="_blank">
+                <img src="<?php echo htmlspecialchars($image); ?>" alt="image">
+        <?php endforeach;?>
+    </div>
+
+</body>
+
+</html>
