cc5ee680d968e76e2ce62ec977569111c403101c
PedroCororatte <pedroagt112@gmail.com>
2023-09-13 22:31:00 -0300

    Update in all screens using bootstrap

--- index.php
+++ index.php
@@ -9,23 +9,66 @@ $images = glob($imageDirectory . "*.{jpg,jpeg,png,gif}", GLOB_BRACE);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css">
-
+    <link rel="stylesheet" type="text/css" href="bstyle.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <title>PhotoVault</title>
 </head>
 
 <body>
-    <div class="gallery">
-        <?php
-        foreach ($images as $image) : ?>
-            <a href="<?php echo htmlspecialchars($image); ?>" target="_blank">
-                <img src="<?php echo htmlspecialchars($image); ?>" alt="image">
-            <?php endforeach; ?>
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Barra Lateral -->
+            <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar">
+                <div class="position-sticky">
 
+                    <ul class="nav flex-column">
+
+                        <p class="display-6 text-primary">
+                            PhotoVault
+                        </p>
+
+                        <li class="nav-item">
+                            <a class="nav-link active text-light" href="index.php">
+                                Inicio
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="upload.php">
+                                Upload
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="#">
+                                Configurações
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <!-- Conteúdo Principal -->
+            <main id="main-content" class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <h2 class="my-4">Galeria de Imagens</h2>
+                <div class="row gallery">
+                    <?php foreach ($images as $image) : ?>
+                        <div class="col-md-2 col-sm-4 mb-1">
+                            <div class="card">
+                                <img src="<?php echo htmlspecialchars($image); ?>" class="card-img-top" alt="Imagem">
+                                <div class="card-body">
+                                    <p class="card-text">Descrição da imagem, autor, etc.</p>
+                                    <a href="<?php echo htmlspecialchars($image); ?>" target="_blank" class="btn btn-primary btn-sm">Ver em Tamanho Completo</a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </main>
+        </div>
     </div>
-    <a href="upload.php" class="upload-button">
-        Upload
-    </a>
 </body>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 </html>
--- upload.php
+++ upload.php
@@ -1,42 +1,110 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="bstyle.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <title>Upload</title>
 </head>
 
 <body>
-    <?php
-    if (isset($_POST['send'])) {
-        $imageDirectory = "images/";
-        $targetFile = $imageDirectory.basename($_FILES["fileToUpload"]["name"]);
-        $tmpFile = $_FILES["fileToUpload"]["tmp_name"];
-        $checkFile = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Barra Lateral -->
+            <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar">
+                <div class="position-sticky">
+                    <ul class="nav flex-column">
+                        <p class="display-6 text-primary">
+                            PhotoVault
+                        </p>
+                        <li class="nav-item">
+                            <a class="nav-link active text-light" href="index.php">
+                                Início
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="upload.php">
+                                Upload
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="#">
+                                Configurações
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
 
-        if ($checkFile !== false) {
-            // Move o arquivo para o diretório de destino
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
-                echo "A imagem " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " foi enviada com sucesso.";
-            } else {
-                echo "Desculpe, houve um erro no envio do arquivo.";
-            }
+            <!-- Conteúdo Principal -->
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div class="d-flex justify-content-center align-items-center flex-column" style="height: 100vh;">
+                    <h1 class="mb-4">Upload de Imagem</h1>
+                    
+                    <!-- Prévia da imagem -->
+                    <div id="imagePreview">
+                        <img id="preview" src="#" alt="Prévia da imagem" class="img-thumbnail mb-4">
+                    </div>
+
+                    <form action="" method="post" enctype="multipart/form-data">
+                        Selecione uma imagem para enviar:
+                        <br><br>
+                        <input type="file" name="fileToUpload" id="fileToUpload" onchange="previewImage()">
+                        <input type="submit" value="Enviar Imagem" name="send" class="btn btn-primary">
+                    </form>
+
+                    <?php
+                    if (isset($_POST['send'])) {
+                        $imageDirectory = "images/";
+                        $targetFile = $imageDirectory . basename($_FILES["fileToUpload"]["name"]);
+                        $tmpFile = $_FILES["fileToUpload"]["tmp_name"];
+                        $checkFile = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+
+                        if ($checkFile !== false) {
+                            // Move o arquivo para o diretório de destino
+                            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
+                                echo '<div class="alert alert-success mt-4" role="alert">A imagem ' . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . ' foi enviada com sucesso.</div>';
+                            } else {
+                                echo '<div class="alert alert-danger mt-4" role="alert">Desculpe, houve um erro no envio do arquivo.</div>';
+                            }
+                        } else {
+                            echo '<div class="alert alert-warning mt-4" role="alert">O arquivo não é uma imagem válida.</div>';
+                        }
+                    }
+                    ?>
+                </div>
+            </main>
+        </div>
+    </div>
+
+    <!-- Inclua os arquivos JavaScript do Bootstrap no final do corpo do documento -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <script>
+    function previewImage() {
+        var preview = document.getElementById('preview');
+        var fileInput = document.getElementById('fileToUpload');
+        var file = fileInput.files[0];
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            preview.src = e.target.result;
+            preview.style.display = 'block'; // Mostrar a prévia da imagem
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
         } else {
-            echo "O arquivo não é uma imagem válida.";
+            preview.src = '#';
+            preview.style.display = 'none'; // Ocultar a prévia da imagem
         }
     }
-    ?>
+</script>
 
-    <form action="" method="post" enctype="multipart/form-data">
-        Selecione uma imagem para enviar:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="Submit" value="Enviar Imagem" name="send">
-    </form>
-    <a href="index.php" class="upload-button">
-        Pagina inicial
-    </a>
 </body>
 
 </html>
